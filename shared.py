46f2a0a3b37542e238520ae663e145cd0bad4d22
kuszaj <>
2018-08-12 20:48:48 +0200

    shared.app -> flask.current_app

--- shared.py
+++ shared.py
@@ -6,7 +6,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["UPLOAD_FOLDER"] = "./uploads"
