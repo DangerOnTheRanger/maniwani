56163b475039c67023dc1820dbd9d4036f5f8049
Kermit Alexander II <kotuxguy@gmail.com>
2018-08-13 23:41:33 -0500

    Add attachment storage API prior to adding S3 support

--- blueprints/upload.py
+++ blueprints/upload.py
@@ -2,7 +2,7 @@ import datetime
 
 from flask import Blueprint, current_app, send_from_directory
 
-from model.Media import Media
+from model.Media import Media, storage
 from shared import db
 
 
@@ -11,13 +11,9 @@ upload_blueprint = Blueprint('upload', __name__, template_folder='template')
 
 @upload_blueprint.route("/<int:media_id>")
 def file(media_id):
-    media = db.session.query(Media).filter(Media.id == media_id).one()
-    return send_from_directory(current_app.config["UPLOAD_FOLDER"], "%d.%s" % (media.id, media.ext),
-                               last_modified=datetime.datetime.now())
+    return storage.get_attachment(media_id)
 
 
 @upload_blueprint.route("/thumb/<int:media_id>")
 def thumb(media_id):
-    thumb_dir = current_app.config["THUMB_FOLDER"]
-    return send_from_directory(thumb_dir, "%d.jpg" % media_id,
-                               last_modified=datetime.datetime.now())
+    return storage.get_thumbnail(media_id)
--- model/NewPost.py
+++ model/NewPost.py
@@ -5,7 +5,7 @@ from PIL import Image
 from flask import current_app, request
 from flask_restful import reqparse, inputs
 
-from model.Media import Media
+from model.Media import Media, storage
 from model.Poster import Poster
 from model.Post import Post
 from model.Reply import Reply, REPLY_REGEXP
@@ -30,7 +30,7 @@ class NewPost:
             poster_hex = gen_poster_id()
             poster = Poster(hex_string=poster_hex, ip_address=ip, thread=thread_id)
             db.session.add(poster)
-            db.session.commit()
+            db.session.flush()
             # bump thread if the poster hasn't posted in this thread before
             should_bump = True
         if args.get("useslip") is True:
@@ -38,42 +38,15 @@ class NewPost:
             if slip and (slip.is_admin or slip.is_mod):
                 poster.slip = slip.id
                 db.session.add(poster)
-                db.session.commit()
         media_id = None
         if "media" in request.files and request.files["media"].filename:
             uploaded_file = request.files["media"]
-            file_ext = uploaded_file.filename.rsplit('.', 1)[1].lower()
-            media = Media(ext=file_ext)
-            db.session.add(media)
-            db.session.commit()
+            media = storage.save_attachment(uploaded_file)
             media_id = media.id
-            full_path = os.path.join(current_app.config["UPLOAD_FOLDER"], "%d.%s" % (media_id, file_ext))
-            uploaded_file.save(full_path)
-            if file_ext != "webm":
-                # non-webm thumbnail generation
-                thumb = Image.open(full_path)
-                if thumb.mode in ("RGBA", "LA"):
-                    background = Image.new(thumb.mode[:-1], thumb.size, (255, 255, 255))
-                    background.paste(thumb, thumb.split()[-1])
-                    thumb = background
-                size = thumb.size
-                scale_factor = 0
-                # width > height
-                if size[0] > size[1]:
-                    scale_factor = 500 / size[0]
-                else:
-                    scale_factor = 500 / size[1]
-                new_width = int(thumb.width * scale_factor)
-                new_height = int(thumb.height * scale_factor)
-                thumb = thumb.resize((new_width, new_height), Image.LANCZOS)
-                thumb.convert("RGB").save(os.path.join(current_app.config["THUMB_FOLDER"], "%d.jpg" % media_id), "JPEG")
-            else:
-                # FIXME: webm thumbnail generation
-                pass
         post = Post(body=body, subject=args["subject"], thread=thread_id, poster=poster.id, media=media_id,
                     spoiler=args["spoiler"])
         db.session.add(post)
-        db.session.commit()
+        db.session.flush()
         replying = re.finditer(REPLY_REGEXP, body)
         if replying:
             for match in replying:
@@ -81,8 +54,8 @@ class NewPost:
                     reply_id = int(raw_reply_id)
                     reply = Reply(reply_from=post.id, reply_to=reply_id)
                     db.session.add(reply)
-                    db.session.commit()
         if should_bump:
             thread = db.session.query(Thread).filter_by(id=thread_id).one()
             thread.last_updated = post.datetime
-            db.session.commit()
+            db.session.add(thread)
+        db.session.commit()
