bd0062cb03b010ce6db0eef8371c655ddeff7689
Kermit Alexander II <kotuxguy@gmail.com>
2018-10-30 15:03:08 -0500

    Fix schema issue for Postgres compatibility

--- model/Poster.py
+++ model/Poster.py
@@ -4,6 +4,6 @@ from shared import db
 class Poster(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     hex_string = db.Column(db.String(4), nullable=False)
-    ip_address = db.Column(db.Integer, nullable=False)
+    ip_address = db.Column(db.String(15), nullable=False)
     thread = db.Column(db.Integer, db.ForeignKey("thread.id", ondelete="CASCADE"), nullable=False)
     slip = db.Column(db.Integer, db.ForeignKey("slip.id"), nullable=True)
