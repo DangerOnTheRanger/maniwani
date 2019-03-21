33888e96ca4d90d9b43843a656cf6269540195f3
Kermit Alexander II <kotuxguy@gmail.com>
2019-03-21 08:09:06 -0500

    Fix update script

--- update.py
+++ update.py
@@ -1,11 +1,14 @@
+import os
+
 from alembic import command
 from alembic.config import Config
 from flask.cli import with_appcontext
 
-from model.Media import storage, MIGRATION_DIR
+from model.Media import storage
 from shared import app, db
 
 
+MIGRATION_DIR = "migrations"
 EARLIEST_REVISION = "b38b893343b7"
 
 
@@ -17,7 +20,7 @@ def update_db():
     alembic_config = Config(os.path.join(MIGRATION_DIR, "alembic.ini"))
     alembic_config.set_main_option("script_location", MIGRATION_DIR)
     with app.app_context():
-        if not db.dialect.has_table(engine, "alembic_version"):
+        if not db.engine.dialect.has_table(db.engine, "alembic_version"):
             # consider old databases lacking alembic support to have the earliest
             # revision
             command.stamp(config=alembic_config, revision=EARLIEST_REVISION)
