8f22ad543a553ae5455b77a7cc93e2ed319a598e
John Dallahan <comeon@getbackinthe.kitchen>
2019-04-23 16:37:20 +0300

    resources: move resources to their own folder

---
+++ resources/__init__.py
@@ -0,0 +1,15 @@
+
+from .BoardCatalog import BoardCatalogResource
+from .BoardList import BoardListResource
+from .Firehose import FirehoseResource
+from .NewPost import NewPostResource
+from .NewThread import NewThreadResource
+from .NewPost import NewPostResource
+from .PostRemoval import PostRemovalResource
+from .ThreadPosts import ThreadPostsResource
+
+__all__ = [
+    "BoardCatalogResource", "BoardListResource", "FirehoseResource",
+    "NewPostResource", "NewThreadResource", "NewPostResource",
+    "PostRemovalResource", "ThreadPostsResource",
+]
