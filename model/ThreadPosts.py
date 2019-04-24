e60d1d9c30948110528549e9f2aead0edcdd1ade
Kermit Alexander II <kotuxguy@gmail.com>
2019-04-24 00:01:38 -0500

    Use worker pool in ThreadPosts to avoid SQL connection saturation

--- model/ThreadPosts.py
+++ model/ThreadPosts.py
@@ -55,12 +55,16 @@ class ThreadPosts:
         return posts
 
     def _json_friendly(self, posts, thread):
-        greenlets = []
+        greenlet_pool = []
         result_queue = gevent.queue.PriorityQueue()
+        post_queue = gevent.queue.Queue()
         for index, post in enumerate(posts):
-            worker = gevent.spawn(post_denormalize_worker, index, post, thread, result_queue)
-            greenlets.append(worker)
-        gevent.joinall(greenlets)
+            post_queue.put((index, post))
+        # TODO: allow worker pool size to be configurable
+        for x in range(5):
+            worker = gevent.spawn(post_denormalize_worker, thread, post_queue, result_queue)
+            greenlet_pool.append(worker)
+        gevent.joinall(greenlet_pool)
         sorted_results = list()
         while not result_queue.empty():
             _, post = result_queue.get()
@@ -68,27 +72,29 @@ class ThreadPosts:
         return sorted_results
 
 
-def post_denormalize_worker(index, post, thread, result_queue):
-    p_dict = dict()
-    p_dict["body"] = post.body
-    p_dict["datetime"] = post.datetime
-    p_dict["id"] = post.id
-    if index == 0:
-        p_dict["tags"] = thread.tags
-    session = db.session
-    poster = session.query(Poster).filter(Poster.id == post.poster).one()
-    p_dict["poster"] = poster.hex_string
-    p_dict["subject"] = post.subject
-    p_dict["media"] = post.media
-    if post.media:
-        media = session.query(Media).filter(Media.id == post.media).one()
-        p_dict["media_ext"] = media.ext
-        p_dict["mimetype"] = media.mimetype
-        p_dict["is_animated"] = media.is_animated
-    p_dict["spoiler"] = post.spoiler
-    p_dict["slip"] = poster.slip
-    p_dict["replies"] = []
-    replies = session.query(Reply).filter(Reply.reply_to == post.id).all()
-    for reply in replies:
-        p_dict["replies"].append(reply.reply_from)
-    result_queue.put((index, p_dict))
+def post_denormalize_worker(thread, post_queue, result_queue):
+    while not post_queue.empty():
+        index, post = post_queue.get()
+        p_dict = dict()
+        p_dict["body"] = post.body
+        p_dict["datetime"] = post.datetime
+        p_dict["id"] = post.id
+        if index == 0:
+            p_dict["tags"] = thread.tags
+        session = db.session
+        poster = session.query(Poster).filter(Poster.id == post.poster).one()
+        p_dict["poster"] = poster.hex_string
+        p_dict["subject"] = post.subject
+        p_dict["media"] = post.media
+        if post.media:
+            media = session.query(Media).filter(Media.id == post.media).one()
+            p_dict["media_ext"] = media.ext
+            p_dict["mimetype"] = media.mimetype
+            p_dict["is_animated"] = media.is_animated
+        p_dict["spoiler"] = post.spoiler
+        p_dict["slip"] = poster.slip
+        p_dict["replies"] = []
+        replies = session.query(Reply).filter(Reply.reply_to == post.id).all()
+        for reply in replies:
+            p_dict["replies"].append(reply.reply_from)
+        result_queue.put((index, p_dict))
